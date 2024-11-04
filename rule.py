f38b48bfd7c62f2f20a2cb2d5427169cd75cbd53
Cbsydhs <cbsydhs@gmail.com>
2024-11-04 11:11:46 +0800

    +

--- rule.py
+++ rule.py
@@ -31,58 +31,41 @@ if __name__ == "__main__":
     rule = {
         "rule" : {
             "urls_list" : [
-                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Telegram/Telegram.list",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Google/Google.list",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/YouTube/YouTube.list",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Twitter/Twitter.list",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Microsoft/Microsoft.list",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/GitHub/GitHub.list",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Python/Python.list",
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Telegram/Telegram.list",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Wikipedia/Wikipedia.list",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Facebook/Facebook.list",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Whatsapp/Whatsapp.list",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Instagram/Instagram.list",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Pixiv/Pixiv.list",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Discord/Discord.list",
-                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Reddit/Reddit.list",
-                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Twitch/Twitch.list",
-                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/PikPak/PikPak.list"
             ],
             "urls_yaml" : [
-                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Telegram/Telegram.yaml",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Google/Google.yaml",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/YouTube/YouTube.yaml",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Twitter/Twitter.yaml",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Microsoft/Microsoft.yaml",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/GitHub/GitHub.yaml",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Python/Python.yaml",
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Telegram/Telegram.yaml",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Wikipedia/Wikipedia.yaml",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Facebook/Facebook.yaml",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Whatsapp/Whatsapp.yaml",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Instagram/Instagram.yaml",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Pixiv/Pixiv.yaml",
                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Discord/Discord.yaml",
-                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Reddit/Reddit.yaml",
-                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Twitch/Twitch.yaml",
-                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/PikPak/PikPak.yaml"
             ],
 
             "ex" : [
-                "DOMAIN-SUFFIX,raw.githubusercontent.com",
                 "DOMAIN-SUFFIX,tradingview.com",
-                "DOMAIN-SUFFIX,metamask.io",
-                "DOMAIN-SUFFIX,coingecko.com",
                 "DOMAIN-SUFFIX,linkedin.com",
-                "DOMAIN-SUFFIX,unsplash.com",
-                "DOMAIN-SUFFIX,archive.org",
-                "DOMAIN-SUFFIX,ycombinator.com",
-                "DOMAIN-SUFFIX,ipfs.io",
-                "DOMAIN-SUFFIX,binance.org",
-                "DOMAIN-SUFFIX,uniswap.org",
-                "DOMAIN-SUFFIX,opensea.io",
                 "DOMAIN-SUFFIX,elliottwave.com",
-                "DOMAIN-SUFFIX,nanoport.xyz",
-                "DOMAIN-SUFFIX,pqjc.sit",
+                "DOMAIN-SUFFIX,pqjc.sit"
             ]
         },
 
@@ -97,12 +80,6 @@ if __name__ == "__main__":
             ],
 
             "ex" : [
-                "DOMAIN-SUFFIX,gemini.google.com",
-                "DOMAIN-SUFFIX,notebooklm.google",
-                "DOMAIN-SUFFIX,apis.google.com",
-                "DOMAIN-SUFFIX,ai.google.dev",
-                "DOMAIN-SUFFIX,aistudio.google.com",
-                "DOMAIN-SUFFIX,generativelanguage.googleapis.com",
                 "DOMAIN-SUFFIX,huggingface.co",
                 "DOMAIN-SUFFIX,perplexity.ai"
             ]
